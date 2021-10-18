17365029e34bb7e0081e194e5caff65c35785857
Bharatchandran <bharatchandrankr@gmail.com>
2021-10-18 17:48:02 +0530

    modified:Day 4 :1

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 
-const App = () => {
-  return <div className="App">Hello World</div>;
-};
+class App extends Component {
+  constructor() {
+    super();
+    console.log("Constructor Called");
+  }
+  state = {
+    Name: "Bharat",
+    Salutation: "Hola",
+  };
 
+  componentDidMount() {
+    console.log("componentDidMount called");
+  }
+  componentDidUpdate() {
+    console.log("componentDidUpdate called");
+  }
+  render() {
+    //DO NOT USE TIMER OR SETTING STATES UINSIDE RENDER FUNCTION
+    if (false && this.state.Salutation !== "Hello")
+      setTimeout(() => {
+        // Do not Mutae the state RERENDER WILL NOT HAPPEN this.state.Name="Hello "\
+        //Change the state after 5 seconds
+        this.setState({
+          Name: "Bharatchandran",
+          Salutation: "Hello",
+        });
+      }, 5000);
+    console.log("render Called!");
+    return (
+      <div className="App">
+        {this.state.Salutation}
+        {this.state.Name}
+      </div>
+    );
+  }
+}
 export default App;
